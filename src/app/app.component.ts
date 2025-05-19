718726531161007506ba725b6a107000d03b61fe
djoudj-dev <nedellec.julien.78@gmail.com>
2025-05-19 20:46:28 +0200

    feat(component:pages): Ajoute la section de contact au portfolio
    - Intègre le composant `ContactComponent` dans la page principale.
    - Initialise le service EmailJS pour la gestion des messages via l'ID utilisateur.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from './shared/ui/navbar/navbar.component';
 import { HeroComponent } from './features/public/hero/hero.component';
 import { AboutComponent } from './features/public/about/about.component';
 import { StacksComponent } from './features/public/stacks/stacks.component';
 import { ProjectComponent } from './features/public/project/project.component';
+import { ContactComponent } from './features/public/contact/contact.component';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, HeroComponent, AboutComponent, StacksComponent, ProjectComponent],
+  imports: [NavbarComponent, HeroComponent, AboutComponent, StacksComponent, ProjectComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-app-portfolio';
+
+  ngOnInit(): void {
+    emailjs.init('RF_1J9PnDw2QM1OIB');
+  }
 }
