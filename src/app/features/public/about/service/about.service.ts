d839857e1074bf47c2fe352613e0f7a670a29962
djoudj-dev <nedellec.julien.78@gmail.com>
2025-05-20 10:59:56 +0200

    refactor(service:api): Remplace HttpClient par DataService pour la section About
    - Adapte le service About pour utiliser DataService au lieu de HttpClient.
    - Met à jour les messages d'erreur pour améliorer leur clarté.
    
    style(component:ui): Ajuste les dimensions des icônes LinkedIn et GitHub
    
    - Augmente la taille des icônes à 48x48 et ajoute l'attribut `priority`.

--- src/app/features/public/about/service/about.service.ts
+++ src/app/features/public/about/service/about.service.ts
@@ -1,20 +1,20 @@
 import { inject, Injectable, signal } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { About } from '../interface/about.interface';
+import { DataService } from '../../../../core/services/data.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AboutService {
-  private readonly http = inject(HttpClient);
+  private readonly dataService = inject(DataService);
 
   private readonly _data = signal<About | null>(null);
   readonly data = this._data.asReadonly();
 
   load(): void {
-    this.http.get<About>('http://localhost:3000/about').subscribe({
+    this.dataService.getSection<About>('about').subscribe({
       next: (res) => this._data.set(res),
-      error: (err) => console.error('Erreur API about:', err),
+      error: (err) => console.error('Erreur chargement about:', err),
     });
   }
 }
