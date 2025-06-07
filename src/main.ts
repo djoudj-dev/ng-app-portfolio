3410c15544cbcb4517696c45b002c5ae80987f58
djoudj-dev <nedellec.julien.78@gmail.com>
2025-06-07 16:01:33 +0200

    refactor(config:build): Simplifie la génération des fichiers d'environnement
    - Écrit directement `environment.prod.ts` dans le Dockerfile au lieu de générer et copier `environment.ts`.
    - Ajoute des logs dans `main.ts` pour indiquer l'environnement actif et l'URL de l'API.

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from '@app/app.config';
 import { AppComponent } from '@app/app.component';
+import { environment } from '@environments/environment';
+
+console.log('✅ Angular is running in:', environment.production ? 'PRODUCTION' : 'DEV');
+console.log('🌍 API Base URL =', environment.apiUrl);
 
 bootstrapApplication(AppComponent, appConfig).catch(() => console.error());
