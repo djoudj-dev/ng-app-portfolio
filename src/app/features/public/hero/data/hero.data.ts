46ea239303b2fc5b5822f93ac64ff42c8674c8ac
djoudj-dev <nedellec.julien.78@gmail.com>
2025-06-07 18:55:32 +0200

    refactor(component:public): Simplifie le composant `hero`
    - Supprime les injections inutilisées liées au suivi et à la gestion des fichiers.
    - Ajoute un chemin direct pour le téléchargement du CV via les données statiques.
    - Réorganise les interfaces et constantes pour clarifier l'accès aux données du composant.

--- src/app/features/public/hero/data/hero.data.ts
+++ src/app/features/public/hero/data/hero.data.ts
@@ -6,4 +6,5 @@ export const DATA_HERO: Hero = {
   jobTitle: 'Développeur web et web mobile',
   jobDescription: `Je conçois des applications web performantes et accessibles, en mettant les besoins des utilisateurs au centre de chaque projet. Mon approche combine exigence technique et sens du design pour des solutions à la fois robustes et élégantes.`,
   illustration: 'images/laptop1.svg',
+  cvPath: 'docs/CV_DEVELOPPEUR_ANGULAR_NEDELLEC_JULIEN.pdf',
 };
